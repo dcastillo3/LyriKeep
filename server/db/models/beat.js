5c6b3768299cf405c5a9a7aea132932cacb54e0a
Daniel Castillo <dcastillo3@gmail.com>
2018-06-05 22:04:01 -0400

    updated readme

---
+++ server/db/models/beat.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Beat = db.define('beat', {
+  
+})
+
+module.exports = Beat
---
+++ server/db/models/section.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Section = db.define('section', {
+  
+})
+
+module.exports = Section
